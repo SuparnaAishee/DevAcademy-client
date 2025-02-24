463976d71f250bf72942750c1316e08f8e98699f
Suparna Dhar <suparnad806@gmail.com>
2025-02-24 15:34:48 +0600

    types

---
+++ app/Types/authTypes.ts
@@ -0,0 +1,21 @@
+export interface LoginData {
+  email: string;
+  password: string;
+}
+
+export interface RegisterData {
+  name: string;
+  email: string;
+  password: string;
+  phone?: string;
+  address?: string;
+}
+
+export interface AuthResponse {
+  accessToken: string;
+  user: {
+    id: string;
+    name: string;
+    email: string;
+  };
+}
